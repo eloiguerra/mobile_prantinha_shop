f89ceb4d94fb2dc2b2b717aafaac999ffe67a96a
eloiguerra <joseeloi.juninho@gmail.com>
2020-12-07 10:35:13 -0300

    more pranta

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import styled from 'styled-components/native'
 
 import api from '../../services/api';
@@ -12,20 +12,32 @@ export default function Home() {
     // console.log('cavalo')
     api.get('/plants')
     .then(response => {
-      console.log(response)
+      // console.log(response.data)
       setPlants(response.data)    
     })
     .catch(error => {
-      console.log(error)
+      // console.log(error)
     })
   }, [])
+  const categories = [
+    {value: 'All', viewValue: 'Todos'},
+    {value: 'carnivorousPlant', viewValue: 'Plantas carnívoras'},
+    {value: 'suculents', viewValue: 'Suculentas'},
+    {value: 'Seeds', viewValue: 'Sementes'},
+  ]
 
   const changeCategory = category => setSelectedCategory(category)
 
   const ProductItem = product => {
     return (
       <Product>
-        <ProductImage source = {product.pathUrl} />
+        <ProductImage source = {{uri:product.path}} />
+        <ProductInfo>
+          <ProductTitle>
+            <Text>{product.name}</Text>
+            <Text>Preço: R${product.price}</Text>
+          </ProductTitle>
+        </ProductInfo>
       </Product>
     )
   }
@@ -34,27 +46,21 @@ export default function Home() {
     <Container>
       <Header>
         <Categories horizontal = {true} showsHorizontalScrollIndicator={false}>
-          <Category>
-            <CategoryName 
-              selected = {selectedCategory === 'All' ? true : false }
-            > 
-              Todos
-            </CategoryName> 
-          </Category>
-          <Category>
-            <CategoryName> Flor </CategoryName> 
-          </Category>
-          <Category> 
-            <CategoryName> Muda </CategoryName>
-          </Category>
-          <Category> 
-            <CategoryName> Semente </CategoryName> 
-          </Category>
+          {categories.map((item, index) => (
+              <Category key = {`category-${index}`} onPress = {() => changeCategory(item.value)}>
+                <CategoryName 
+                  selected = {selectedCategory === item.value ? true : false }
+                > 
+                  {item.viewValue}
+                </CategoryName> 
+                {/* {selectedCategory === category && <SelectedCategory />} */}
+              </Category>
+          ))}
         </Categories>
       </Header>
       <Products 
         data = {plants} 
-        keyExtractor = {item => String(item.plantId)}
+        keyExtractor = {item => String(item.id)}
         renderItem = {({item}) => ProductItem(item)} 
       />
     </Container>
@@ -83,13 +89,41 @@ const Category = styled.TouchableOpacity`
   height: 32px;
 `;
 
-const CategoryName = styled(Text)``;
+/* const SelectedCategory =  styled.View`
+  height: 1px;
+  width: 100%;
+  border-radius: 50px;
+  color: #52b788;
+`; */
+
+const CategoryName = styled(Text)`
+  color: ${props => props.selected ? '#6a994e' : '#000000'}; 
+`;
 
 const Products = styled.FlatList`
   margin-top: 32px;
   flex: 1;
 `;
 
-const Product = styled.TouchableOpacity``;
+const Product = styled.TouchableOpacity`
+  margin-bottom: 32px;
 
-const ProductImage = styled.Image``;
+  background: #ffffff;
+`;
+
+const ProductImage = styled.Image`
+  height: 300px;
+  width: 100%;
+`;
+
+const ProductInfo = styled.View`
+  padding: 8px;
+  border-radius: 8px;
+
+  align-items: center;
+`;
+
+
+const ProductTitle = styled.View`
+
+`;
